4c4e685bf1601c4f47100d1b9da75ccdc7008f4a
Thomas Kirschstein <thomas.kirschstein@imw.fraunhofer.de>
2024-06-07 14:10:33 +0200

    new com

--- tmp.R
+++ tmp.R
@@ -7,3 +7,18 @@ newsvend.func <- function(q = 120, cu = 2, co = 0.5, mu = 100, sigma = 20){
 }
 
 newsvend.func(q = 120, cu = 1, co = .75, mu = 100, sigma = 30 )
+
+
+# Formulate a loop that calculates the inventory records over n periods based on an initial stock level (say  i=20) where every 4 periods 40 units arrive at the inventory. Sample the demand for each period from a normal distribution with D ∼ N(10, 2) and round to integers.
+
+# number of periods
+n <- 100
+i.0 <- 20
+
+d.vec <- rnorm(n = n, mean = 10, sd = 2)
+d.vec <- round(d.vec, digits = 0)
+
+for(){
+  
+  
+}
